688fb3bb4b3cf26da74d3f94ace945b92a7f49b1
Dekespo <dekespo@gmail.com>
2018-04-16 20:06:45 +0200

    First android project

---
+++ app/src/main/java/com/dekespo/remotecomputer/RemoteComputerActivity.java
@@ -0,0 +1,15 @@
+package com.dekespo.remotecomputer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RemoteComputerActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote_computer);
+    }
+}
