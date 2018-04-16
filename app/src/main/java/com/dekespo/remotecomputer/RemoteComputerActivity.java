8e9233b6ec56eb4946ebee625888e6ae3b473c3a
Dekespo <dekespo@gmail.com>
2018-04-16 20:37:44 +0200

    closes #2 added Button in UI

--- app/src/main/java/com/dekespo/remotecomputer/RemoteComputerActivity.java
+++ app/src/main/java/com/dekespo/remotecomputer/RemoteComputerActivity.java
@@ -2,6 +2,8 @@ package com.dekespo.remotecomputer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RemoteComputerActivity extends AppCompatActivity
 {
@@ -11,5 +13,13 @@ public class RemoteComputerActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote_computer);
+
+        Button button = findViewById(R.id.bluetooth_button);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+            }
+        });
     }
 }
