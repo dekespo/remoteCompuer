f8ffed4588c38c959d562746a670185361a4ad66
Dekespo <dekespo@gmail.com>
2018-05-06 15:36:04 +0200

    closes #30 Added first mouse class in the server

---
+++ javaServer/src/main/java/com/dekespo/javaserver/monitor/Mouse.java
@@ -0,0 +1,49 @@
+package com.dekespo.javaserver.monitor;
+
+import java.awt.AWTException;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Robot;
+
+public class Mouse {
+
+  private Robot robot;
+  private Point currentPosition;
+
+  public Mouse() {
+    this.currentPosition = MouseInfo.getPointerInfo().getLocation();
+    try {
+      this.robot = new Robot();
+    } catch (AWTException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void move(MouseDirections direction, int speed) {
+    int x = (int) this.currentPosition.getX();
+    int y = (int) this.currentPosition.getY();
+    switch (direction) {
+      case UP:
+        this.robot.mouseMove(x, y + speed);
+        break;
+      case DOWN:
+        this.robot.mouseMove(x, y - speed);
+        break;
+      case RIGHT:
+        this.robot.mouseMove(x + speed, y);
+        break;
+      case LEFT:
+        this.robot.mouseMove(x - speed, y);
+        break;
+    }
+    this.currentPosition = MouseInfo.getPointerInfo().getLocation();
+    System.out.println(this.currentPosition);
+  }
+
+  public enum MouseDirections {
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT
+  }
+}
