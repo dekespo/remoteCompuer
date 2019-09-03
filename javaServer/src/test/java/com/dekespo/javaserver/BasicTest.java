b896dca964cf1986c87b392fe20fbdcc24907038
Dekespo <dekespo@gmail.com>
2019-09-03 07:02:01 +0200

    More on circle ci work with java test

---
+++ javaServer/src/test/java/com/dekespo/javaserver/BasicTest.java
@@ -0,0 +1,12 @@
+package com.dekespo.javaserver;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BasicTest {
+  @Test
+  public void addition_isCorrect() throws Exception {
+    assertEquals(4, 2 + 3);
+  }
+}
